8359775e61fbf690527cd0c541d90865305350f1
Anish Upreti <upretianish407@.com>
2026-05-06 20:06:58 +0545

    aba hundred bhayo lah

--- src/features/auth/pages/GoogleSuccess.jsx
+++ src/features/auth/pages/GoogleSuccess.jsx
@@ -6,18 +6,21 @@ const GoogleSuccess = () => {
     const navigate = useNavigate();
     const [searchParams] = useSearchParams();
 
+
+    const setCookies = async () => {
+        axios.post(`${import.meta.env.VITE_FRONTEND_URL}/api/auth/google/set-cookie`,
+            { token },
+            { withCredentials: true } // ← so the cookie gets stored
+        )
+            .then(() => navigate('/', { replace: true }))
+            .catch(() => navigate('/login', { replace: true }));
+    }
     useEffect(() => {
         const token = searchParams.get("token");
-        console.log(token)
 
         if (token) {
             // Send token to backend — let IT set the cookie
-            axios.post(`${import.meta.env.VITE_FRONTEND_URL}/api/auth/google/set-cookie`,
-                { token },
-                { withCredentials: true } // ← so the cookie gets stored
-            )
-                .then(() => navigate('/', { replace: true }))
-                .catch(() => navigate('/login', { replace: true }));
+            setCookies()
         } else {
             navigate('/login', { replace: true });
         }
